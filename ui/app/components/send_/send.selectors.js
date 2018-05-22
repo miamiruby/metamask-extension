2a90c71dd4af2801db34ff7a651f6d93500cd009
Dan <danjm.com@gmail.com>
2018-05-22 05:40:06 -0230

    Simplify gas estimate actions and add local estimateGasPriceFromRecentBlocks method.

--- ui/app/components/send_/send.selectors.js
+++ ui/app/components/send_/send.selectors.js
@@ -3,6 +3,9 @@ const abi = require('human-standard-token-abi')
 const {
   multiplyCurrencies,
 } = require('../../conversion-util')
+const {
+  estimateGasPriceFromRecentBlocks,
+} = require('./send.utils')
 
 const selectors = {
   accountsWithSendEtherInfoSelector,
@@ -18,8 +21,10 @@ const selectors = {
   getForceGasMin,
   getGasLimit,
   getGasPrice,
+  getGasPriceFromRecentBlocks,
   getGasTotal,
   getPrimaryCurrency,
+  getRecentBlocks,
   getSelectedAccount,
   getSelectedAddress,
   getSelectedIdentity,
@@ -125,6 +130,10 @@ function getGasPrice (state) {
   return state.metamask.send.gasPrice
 }
 
+function getGasPriceFromRecentBlocks (state) {
+  return estimateGasPriceFromRecentBlocks(state.metamask.recentBlocks)
+}
+
 function getGasTotal (state) {
   return state.metamask.send.gasTotal
 }
@@ -132,6 +141,10 @@ function getGasTotal (state) {
 function getPrimaryCurrency (state) {
   const selectedToken = getSelectedToken(state)
   return selectedToken && selectedToken.symbol
+}
+
+function getRecentBlocks (state) {
+  return state.metamask.recentBlocks
 }
 
 function getSelectedAccount (state) {
